8219e571e4377850ce92d3a6a88da592ba8fe847
Julio Garrido Vico <55106204+Juliogarridovico@users.noreply.github.com>
2022-11-12 18:01:28 +0100

    funciona sin pinia

---
+++ src/stores/counter.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: () => ({
+    counter: 0,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+    isEvenOrOdd: (state) => (state.counter % 2 ? "Odd" : "Even"),
+  },
+  actions: {
+    increment() {
+      this.counter += 1;
+    },
+
+    decrement() {
+      this.counter -= 1;
+    },
+  },
+});
---
+++ src/views/Game.vue
@@ -0,0 +1,73 @@
+<script>
+// import { useCounterStore } from "../stores/counter.js";
+
+// export default {
+//   data() {
+//     return {
+//       clicks: 0,
+//     };
+//   },
+//   setup() {
+//     const counter = useCounterStore();
+
+//     counter.count++;
+//     // with autocompletion ✨
+//     counter.$patch({ count: counter.count + 1 });
+//     // or using an action instead
+//     counter.increment();
+//   },
+// };
+
+import { useCounterStore } from "@/stores/counter.js";
+import { mapState } from "pinia";
+export default {
+  name: "theGame",
+  data() {
+    return {
+      clicks: 0,
+    };
+  },
+  computed: {
+    ...mapState(useCounterStore, ["counter"]),
+  },
+};
+</script>
+
+<template>
+  <div class="theGame">
+    <p>click {{ clicks }}</p>
+    <p>Pinia {{ counter }}</p>
+    <button v-on:click="clicks += 1">vueJS</button>
+    <button v-on:click="useCounterStore.increment()">Pinia</button>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Gloria+Hallelujah");
+
+button {
+  border-radius: 100%;
+  width: 150px;
+  height: 150px;
+  background-color: #f4e5b5;
+  border: solid #635425;
+  box-shadow: 0px 6px #77725e;
+  font-family: "Gloria Hallelujah", cursive;
+  font-size: 18px;
+  color: #593210;
+}
+
+button:hover {
+  background-color: #c1b48b;
+}
+
+button:active {
+  background-color: #c1b48b;
+  box-shadow: 0px 2px #494537;
+  transform: translateY(4px);
+}
+
+button:focus {
+  outline: 0;
+}
+</style>
