08eddba4ef398df977fae23cd5c11fc530868900
Julio Garrido Vico <55106204+Juliogarridovico@users.noreply.github.com>
2022-11-16 18:03:44 +0100

    hacer que funcine las routas en netlify

--- src/router/index.js
+++ src/router/index.js
@@ -23,13 +23,7 @@ const router = createRouter({
   ]
 })
 
-const router = createRouter({
-  history: createWebHistory("/"),
-  routes,
-  scrollBehavior() {
-    document.getElementById("app").scrollIntoView();
-  },
-});
+
 
 export default router;
 
