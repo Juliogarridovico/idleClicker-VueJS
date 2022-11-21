1f2369c7784aa0bde3fa49ada8aba3029ebf56ac
Julio Garrido Vico <55106204+Juliogarridovico@users.noreply.github.com>
2022-11-21 13:54:35 +0100

    lo basico finalizado

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,16 +4,15 @@ export const useCounterStore = defineStore({
   id: "counter",
   state: () => ({
     counter: 0,
-    clicksTotales:1,
+    
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2,
-    isEvenOrOdd: (state) => (state.counter % 2 ? "Odd" : "Even"),
+    
   },
   actions: {
-    increment(click) {
-      console.log("estoy");
-      this.counter += 1;
+    increment() {
+      this.counter=+1;
+      localStorage.setItem("clicks", this.counter);
     },
   },
 });
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,20 +7,20 @@ export default {
   data() {
     return {
       clicks: 0,
-      clicksTotales: 0,
+
       ganaciaSecundo: 0,
       mejoras: [
-        { name: "clickUsuario", level: 1, costeInicial: 1 },
-        { name: "mejora_1", level: 0, precompra: 0, costeInicial: 1 },
-        { name: "mejora_2", level: 0, precompra: 0, costeInicial: 2 },
-        { name: "mejora_3", level: 0, precompra: 0, costeInicial: 3 },
-        { name: "mejora_4", level: 0, precompra: 0, costeInicial: 4 },
-        { name: "mejora_5", level: 0, precompra: 0, costeInicial: 5 },
-        { name: "mejora_6", level: 0, precompra: 0, costeInicial: 6 },
-        { name: "mejora_7", level: 0, precompra: 0, costeInicial: 7 },
-        { name: "mejora_8", level: 0, precompra: 0, costeInicial: 8 },
-        { name: "mejora_9", level: 0, precompra: 0, costeInicial: 9 },
-        { name: "mejora_10", level: 0, precompra: 0, costeInicial: 10 },
+        { name: "clickUsuario", level: 1, costeInicial: 0 },
+        { name: "upgrade_1", level: 0, precompra: 0, costeInicial: 1 },
+        { name: "upgrade_2", level: 0, precompra: 0, costeInicial: 2 },
+        { name: "upgrade_3", level: 0, precompra: 0, costeInicial: 3 },
+        { name: "upgrade_4", level: 0, precompra: 0, costeInicial: 4 },
+        { name: "upgrade_5", level: 0, precompra: 0, costeInicial: 5 },
+        { name: "upgrade_6", level: 0, precompra: 0, costeInicial: 6 },
+        { name: "upgrade_7", level: 0, precompra: 0, costeInicial: 7 },
+        { name: "upgrade_8", level: 0, precompra: 0, costeInicial: 8 },
+        { name: "upgrade_9", level: 0, precompra: 0, costeInicial: 9 },
+        { name: "upgrade_10", level: 0, precompra: 0, costeInicial: 10 },
       ],
     };
   },
@@ -31,46 +31,58 @@ export default {
       this.clicks = 0;
     },
     sube: function () {
-      test.increment(this.mejoras[0].level);
-      localStorage.setItem("clicks", test.counter);
-      localStorage.setItem("clicks", test.clicksTotales);
-      this.clicksTotales = test.clicksTotales;
-      this.clicks = test.counter;
+      //test.increment();
+      let suma;
+      suma = localStorage.getItem("clicks");
+      ++suma;
+      localStorage.setItem("clicks", suma);
+      this.clicks = localStorage.getItem("clicks");
+      console.log(suma);
     },
     mejoraJugador: function (mejora) {
-          
       if (this.calcularPrecio(mejora) <= this.clicks) {
         let proximaMejora = mejora + 1;
-        this.mejoras[proximaMejora].precompra = 1;
+        // this.mejoras[proximaMejora].precompra = 1;
         this.mejoras[mejora].level++;
-        this.mejoras[mejora].precompra = 0;
-        this.clicks = this.clicks - this.calcularPrecio(mejora)
+        // this.mejoras[mejora].precompra = 0;
+        this.clicks = this.clicks - this.calcularPrecio(mejora);
+        localStorage.setItem("clicks", this.clicks);
       }
-     console.log(this.calcularPrecio(mejora))
+      // console.log(this.calcularPrecio(mejora));
     },
     incrementoAutomatico: function () {
-      this.clicks = this.clicks * 1.01;
-      let incrementoTotal =0
+      let clicks;
+      let autoincremento;
+      let incrementoTotal = 0;
       this.mejoras.forEach((mejora) => {
-        if (mejora.level > 0) {
-          //
+        if (mejora.level >= 1) {
+          incrementoTotal =
+            (incrementoTotal + this.calcularPrecio(mejora.level)) * 0.01;
         }
       });
+       clicks = localStorage.getItem("clicks");
+      autoincremento = parseFloat(clicks)+incrementoTotal
+      clicks = localStorage.setItem("clicks", autoincremento);
+      // clicks = localStorage.getItem("clicks");
+      this.clicks = localStorage.getItem("clicks");
+       console.log(autoincremento);
     },
-    calcularPrecio:function(mejora){
+    calcularPrecio: function (mejora) {
       //((Precio base/siguiente nivel)*1.07)+precio base+(siguiente nivel*1.07)
-      //name: "clickUsuario", level: 1, costeInicial: 1 
-      // console.log("Test " + this.mejoras[mejora].level); 
-      let coste 
-      coste =((this.mejoras[mejora].costeInicial/(this.mejoras[mejora].level+1))*1.07)+this.mejoras[mejora].costeInicial+((this.mejoras[mejora].level+1)*1.07);
+      //name: "clickUsuario", level: 1, costeInicial: 1
+      // console.log("Test " + this.mejoras[mejora].level);
+      let coste;
+      coste =
+        (this.mejoras[mejora].costeInicial / (this.mejoras[mejora].level + 1)) *
+          1.07 +
+        this.mejoras[mejora].costeInicial + 
+        (this.mejoras[mejora].level + 1) * 1.07;
       return coste;
     },
   },
   created() {},
   mounted() {
-    console.log("pienso luego existo");
     this.clicks = localStorage.getItem("clicks");
-    this.clicksTotales = localStorage.setItem("clicks", test.clicksTotales);
     setInterval(this.incrementoAutomatico, 1000);
   },
 };
@@ -78,31 +90,29 @@ export default {
 
 <template>
   <div class="theGame">
-    <p>Clicks totales: {{ clicksTotales }}</p>
-    <p>click: {{ clicks }}</p>
-    <p v-on:click="resetClicks()">reset clicks</p>
-    <p>Ganancia por segundo: {{ ganaciaSecundo }}</p>
-    <p>nivel del click del usuario: {{ mejoras[0].level }}</p>
+    <p></p>
 
-    <button v-on:click="sube()">Pinia</button>
+    <p>You have {{ clicks }} Clicks</p>
+    <p v-on:click="resetClicks()">reset clicks</p>
+
+    <button v-on:click="sube()">Press this button to get clicks</button>
   </div>
   <div>
+    <h1>You can use your clicks to improve your button</h1>
+    <p>
+      by improving the button you can get clicks automatically while you are
+      playing
+    </p>
     <ul id="bucle" v-for="(mejora, i) in mejoras" :key="i">
-      <!-- <li v-if="mejora.level  >= (mejoras[i].level)+0" @click="mejoraJugador(i)">
-        {{ mejora.name }} ({{ mejora.level }})
-        test {{(mejoras[i].level)+1}}
-      </li> -->
-      <li v-if="mejora.level > 0" @click="mejoraJugador(i)">
-        {{ mejora.name }} ({{ mejora.level }})
-      </li>
-      <li v-if="mejora.precompra == 1" @click="mejoraJugador(i)">
-        test: {{ mejora.name }} ({{ mejora.level }})
+      <li @click="mejoraJugador(i)">
+        level now: {{ mejora.level }} name: {{ mejora.name }} Cost of upgrade:
+        {{ mejora.costeInicial }}
       </li>
     </ul>
   </div>
 </template>
 
-<style >
+<style>
 @import url("https://fonts.googleapis.com/css?family=Gloria+Hallelujah");
 
 button {
@@ -131,4 +141,4 @@ button:focus {
   outline: 0;
 }
 </style>
-// 🥳 
+// 🥳
